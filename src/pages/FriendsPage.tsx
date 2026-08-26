42ef0f21867733716cf0fc6577f6f4e01cc40232
callmedugan <callmedugan@gmail.com>
2026-08-25 17:35:51 -0700

    revised friends page for missing modal

--- src/pages/FriendsPage.tsx
+++ src/pages/FriendsPage.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import AddFriendModal from "../components/friends/AddFriendModal";
 import PageContainer from "./PageContainer";
 import FriendsSplitView from "../components/friends/FriendsSplitView";
 
 export function FriendsPage() {
-	const [showAddFriendModal, setShowAddFriendModal] = useState(false);
-
 	/* ========================================================================= */
 	//                        page
 	/* ========================================================================= */
@@ -13,22 +9,6 @@ export function FriendsPage() {
 	return (
 		<PageContainer title="Your friends" description="See who's free to hang out.">
 			<FriendsSplitView />
-
-			{showAddFriendModal && <AddFriendModal onClose={() => setShowAddFriendModal(false)} />}
 		</PageContainer>
 	);
-
-	// function showAddButton() {
-	// 	return (
-	// 		<button
-	// 			type="button"
-	// 			onClick={() => {
-	// 				setShowAddFriendModal(true);
-	// 			}}
-	// 			className="btn-primary"
-	// 		>
-	// 			Find friend
-	// 		</button>
-	// 	);
-	// }
 }
